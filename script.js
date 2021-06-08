d7fc542321f6b99fc7277b788d11ddc3a67614ed
maurya512 <patelmaurya0512@gmail.com>
2021-06-07 23:43:13 -0400

    added functionality to the sidebar and hamburger menu

---
+++ script.js
@@ -0,0 +1,5 @@
+
+// addEventListener for the hamburger menu
+document.querySelector(".hamburger-menu").addEventListener("click", () => {
+    document.querySelector(".container").classList.toggle("change");
+});
