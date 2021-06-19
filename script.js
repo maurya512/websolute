c98e594a3f7f59d2fce66f5c546d7c19d0310a7f
maurya512 <patelmaurya0512@gmail.com>
2021-06-19 00:22:22 -0400

    added query selector for smooth scroll of the page

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+// sets the timeout for the loading of the page
+window.onload = () => {
+    setTimeout(() => {
+        document.querySelector('body').classList.add('display');
+    }, 4000);
+};
 
 // addEventListener for the hamburger menu
 document.querySelector(".hamburger-menu").addEventListener("click", () => {
     document.querySelector(".container").classList.toggle("change");
+});
+
+// adding an event listener to the scroll button to ensure smooth scrolling of the page
+document.querySelector('.scroll-btn').addEventListener('click', () => {
+    document.querySelector('html').style.scrollBehavior = 'smooth';
+    setTimeout(() => {
+        document.querySelector('html').style.scrollBehavior = 'unset';
+    }, 1000);
 });
